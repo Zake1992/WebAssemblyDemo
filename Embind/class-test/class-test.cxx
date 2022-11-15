a0fa5ecf31ba26d80cb35a41595e867049635ff2
zhangkun <1965525107@qq.com>
2022-11-15 19:36:42 +0800

    embind绑定类的另一种方式

---
+++ Embind/class-test/class-test.cxx
@@ -0,0 +1,46 @@
+#include <iostream>
+// #include "myClass.h"
+
+#include "emscripten/bind.h"
+
+
+// using namespace std; 
+// #include "emscripten/bind.h"
+using namespace emscripten;
+
+
+class ClassTest
+{
+public:
+    ClassTest(int x, std::string y)
+        : x(x), y(y)
+    {
+    }
+
+    void incrementX()
+    {
+        ++x;
+    }
+
+    int getX() const { return x; }
+    void setX(int x_) { x = x_; }
+
+    static std::string getStringFromInstance(const ClassTest &instance)
+    {
+        return instance.y;
+    }
+
+private:
+    int x;
+    std::string y;
+};
+
+// Binding code
+EMSCRIPTEN_BINDINGS(my_class_example)
+{
+    class_<ClassTest>("ClassTest")
+        .constructor<int, std::string>()
+        .function("incrementX", &ClassTest::incrementX)
+        .property("x", &ClassTest::getX, &ClassTest::setX)
+        .class_function("getStringFromInstance", &ClassTest::getStringFromInstance);
+}
---
+++ Embind/class-test/readme.md
@@ -0,0 +1,4 @@
+class-test.cxx编译命令
+```
+emcc --bind -o class-test.js class-test.cxx
+```
